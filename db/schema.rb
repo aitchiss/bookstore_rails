66d613fef09f761ad899810009ac06cbfeecb37d
Suzanne <suzanne.aitchison@gmail.com>
2017-05-09 11:33:26 +0100

    Added bookstore model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170509102022) do
+ActiveRecord::Schema.define(version: 20170509103159) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "author_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "books", ["author_id"], name: "index_books_on_author_id"
+
+  create_table "bookstores", force: :cascade do |t|
+    t.string   "name"
+    t.string   "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,13 @@
+Book.delete_all
 Author.delete_all
 
 a1 = Author.create({name: 'Dan Brown'})
 a2 = Author.create({name: 'Charles Dickens'})
 a3 = Author.create({name: 'JK Rowling'})
+
+b1 = Book.create({author: a1, title: 'The Da Vinci Code'})
+b2 = Book.create({author: a1, title: 'Inferno'})
+
+b3 = Book.create({author: a2, title: 'Hard Times'})
+
+b4 = Book.create({author: a3, title: 'The Casual Vacancy'})
